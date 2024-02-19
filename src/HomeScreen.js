a091568bba4cba83ae5d81a2e0be3dda34776105
Jamal Ahamad <jamalahamad@Jamals-MacBook-Pro.local>
2024-02-19 21:37:09 +0530

    first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+const HomeScreen = () => {
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'green'}}>
+        <Text>HomeScreen</Text>
+      </View>
+    )
+  }
+
+export default HomeScreen
